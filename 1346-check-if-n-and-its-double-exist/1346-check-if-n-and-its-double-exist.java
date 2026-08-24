85d9d7a64637cea23a562e2c5449b1008deb3dcd
Ashish Patel <117652257+Ashishpatel4047@users.noreply.github.com>
2026-08-24 15:05:14 +0530

    Time: 2 ms (97.4%), Space: 44.6 MB (69.06%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for( int nums:arr){
+            if(set.contains(2*nums) ||(set.contains(nums/2)&&(nums%2==0))){
+                return true;
+            }
+            set.add(nums);
+        }
+        return false;
+
+
+        
+    }
+}
